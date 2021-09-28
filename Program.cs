e916edbfcd38e400efe247e9cfbfd62543e62152
coffie2000us <charlescoffie.cri@gmail.com>
2021-09-28 12:28:09 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b; 
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+        }
+    }
+}
